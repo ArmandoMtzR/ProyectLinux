c5d7263195e619348e7a7c369a85e66bb962a221
ArmandoMtzR <h.armando.mtz@outlook.com>
2023-03-27 23:17:28 -0600

    Se agrego el juego para la terminal

--- Codigo/comandos/creditos.sh
+++ Codigo/comandos/creditos.sh
@@ -4,7 +4,6 @@ function creditos() {
   echo "-----------------------------------------"
   echo "Proyecto Terminal Personalizada"
   echo "Desarrollado por:"
-  echo "Sotelo Perez Erika Xitlaly"
   echo "Martínez Rosales Hugo Armando"
   echo "-----------------------------------------"
 }
--- Codigo/comandos/juego.sh
+++ Codigo/comandos/juego.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
 function juego() {
-  
+# Genera un número aleatorio entre 1 y 100
+numero_secreto=$(shuf -i 1-100 -n 1)
+
+# Inicializa el número de intentos
+intentos=0
+
+# Bucle principal del juego
+while true; do
+    # Pide al usuario que introduzca un número
+    printf "Adivina el número secreto (1-100): "
+    read -r numero_usuario
+
+    # Incrementa el número de intentos
+    intentos=$((intentos + 1))
+
+    # Comprueba si el número introducido por el usuario es igual al número secreto
+    if [ "$numero_usuario" -eq "$numero_secreto" ]; then
+        echo "¡Felicidades! Has adivinado el número secreto en $intentos intentos."
+        break
+    elif [ "$numero_usuario" -lt "$numero_secreto" ]; then
+        echo "El número secreto es mayor que $numero_usuario."
+    else
+        echo "
 }
